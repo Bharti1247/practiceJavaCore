7ce7b1b67f20ef00e0a6a01c6ee28272557305ae
Bharti <bhartipandey.95@gmail.com>
2025-06-17 20:12:36 +0530

    Adding string code

---
+++ Practice/src/com/learn/string/TestNull.java
@@ -0,0 +1,15 @@
+package com.learn.string;
+
+public class TestNull {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String str1 = null;
+		String str2 = "null";
+		System.out.println("str1 : " + str1);
+		System.out.println("str2 : " + str2);
+		System.out.println((str1 == str2));
+		System.out.println(str1.equals(str2));
+	}
+
+}
