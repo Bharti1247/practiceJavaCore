9650923b777eb3517c2945158234ed96d35ad258
Bharti <bhartipandey.95@gmail.com>
2025-06-29 19:52:06 +0530

    flatmap ques

---
+++ Practice/src/com/learn/java8/streams/map/FlatMap.java
@@ -0,0 +1,31 @@
+package com.learn.java8.streams.map;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class FlatMap {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		List<List<String>> list = Arrays.asList(
+			Arrays.asList("Lazy Fox", "eating grapes"),
+			Arrays.asList("Lazy Fox", "making money"),
+			Arrays.asList("White sheep", "eating grass", "roaming freely"),
+			Arrays.asList("Students are studying", "in school")
+		);
+		
+		// print list of string
+		list.stream().forEach(System.out::println);
+		System.out.println();
+		
+		// print strings
+		list.stream().flatMap(x -> x.stream()).forEach(System.out::println);
+		System.out.println();
+		
+		// print distinct strings
+		list.stream().flatMap(x -> x.stream()).distinct().forEach(System.out::println);
+
+	}
+
+}
